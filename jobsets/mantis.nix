56d66d303aa3c5e4c9b7e7be79c44b4415ac84f5
Kris Jenkins <krisajenkins@gmail.com>
2018-06-11 17:27:28 +0100

    WIP: Building mantis (and deps) on Nixos.

--- jobsets/mantis.nix
+++ jobsets/mantis.nix
@@ -1,22 +1,32 @@
-{ stdenv, mantisSrc, scala, sbt, callPackage, unzip, sbtVerify }:
+{ stdenv, pkgs, mantisSrc, sbtVerify, protobuf3_5 }:
 
 stdenv.mkDerivation {
   name = "mantis";
-  requiredSystemFeatures = [ "ubuntu" ];
   src = mantisSrc;
 
-  buildInputs = [ scala sbt sbtVerify unzip ];
+  buildInputs = with pkgs; [ scala sbt sbtVerify unzip protobuf3_5 openjdk8 ];
 
   outputs = [ "out" "zip" ];
 
   configurePhase = ''
     export HOME="$NIX_BUILD_TOP"
-    cp -r ${sbtVerify} target
-    chmod -R u+w target
+    cp -r ${sbtVerify}/.ivy .
+    cp -r ${sbtVerify}/.sbt .
+    cp -r ${sbtVerify}/target .
+    chmod -R u+w .ivy .sbt target
+
+    # Get sbt to pre-fetch its dependencies. The cleanest way I've
+    # found of doing this is to get it to list the available projects.
+    sbt -Dsbt.global.base=.sbt/1.0 -Dsbt.ivy.home=.ivy projects
+
+    # We have to patch the executable embedded inside protoc-jar. :-(
+    mkdir -p bin/3.5.1
+    cp ${protobuf3_5}/bin/protoc  bin/3.5.1/protoc-3.5.1-linux-x86_64.exe
+    jar uf .ivy/cache/com.github.os72/protoc-jar/jars/protoc-jar-3.5.1.jar  bin/3.5.1/protoc-3.5.1-linux-x86_64.exe
   '';
 
   buildPhase = ''
-    sbt 'set test in Test := {}' dist
+    sbt -Dsbt.global.base=.sbt/1.0 -Dsbt.ivy.home=.ivy 'set test in Test := {}' dist
   '';
 
   installPhase = ''
